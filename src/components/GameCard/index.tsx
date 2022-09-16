413b44498a3abecc4a9432ae686bfb55014d32b1
Giselle Hoekveld <gisellehoekveld@gmail.com>
2022-09-15 23:52:08 -0300

    update game card interface

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -12,9 +12,11 @@ import { THEME } from '../../theme';
 
 export interface GameCardProps {
   id: string;
-  name: string;
-  ads: string;
-  cover: ImageSourcePropType;
+  title: string;
+  _count: {
+    ads: number
+  };
+  bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps{
@@ -26,7 +28,7 @@ export function GameCard({ data, ...rest }: Props) {
     <TouchableOpacity style={styles.container} {...rest}>
       <ImageBackground
         style={styles.cover}
-        source={data.cover}
+        source={{uri: data.bannerUrl}}
       >
         <LinearGradient 
           colors={THEME.COLORS.FOOTER}
@@ -34,11 +36,11 @@ export function GameCard({ data, ...rest }: Props) {
         >
           
           <Text style={styles.name}>
-            {data.name}
+            {data.title}
           </Text>
 
           <Text style={styles.ads}>
-            {data.ads} anúncios
+            {data._count.ads} anúncios
           </Text>
 
         </LinearGradient>
