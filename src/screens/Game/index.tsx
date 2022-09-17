075c739c7340fcf37de1d6f635092518544961bd
Giselle Hoekveld <gisellehoekveld@gmail.com>
2022-09-17 19:53:56 -0300

    create clipboard function to copy discord id

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -24,7 +24,7 @@ interface RouteParams {
 
 export function Game() {
   const [duos, setDuos] = useState<DuoCardProps[]>([]);
-  const [discordDuoSelected, setDiscordDuoSelected] = useState('gi')
+  const [discordDuoSelected, setDiscordDuoSelected] = useState('');
 
   const navigation = useNavigation();
   const route = useRoute();
@@ -33,11 +33,17 @@ export function Game() {
   function handleGoBack() {
     navigation.goBack();
   }
+
+  async function getDiscordUser(adsId: string) {
+    fetch(`http://192.168.100.135:3333/ads/${adsId}/discord`)
+      .then(response => response.json())
+      .then(data => setDiscordDuoSelected(data.discord))
+  }
   
   useEffect(() => {
     fetch(`http://192.168.100.135:3333/games/${game.id}/ads`)
-    .then(response => response.json())
-    .then(data => setDuos(data));
+      .then(response => response.json())
+      .then(data => setDuos(data));
   }, []);
 
   return (
@@ -78,7 +84,7 @@ export function Game() {
           renderItem={({item}) => (
             <DuoCard 
               data={item}
-              onConnect={() => {}}
+              onConnect={() => getDiscordUser(item.id)}
             />
           )}
           horizontal
@@ -95,7 +101,7 @@ export function Game() {
 
         <DuoMatch 
           visible={discordDuoSelected.length > 0}
-          discord="giselle#7777"
+          discord={discordDuoSelected}
           onClose={() => setDiscordDuoSelected('')}
         />
       </SafeAreaView>
